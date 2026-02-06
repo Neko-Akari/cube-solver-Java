09745aced1190a4a22df78b362c13b0a0a8243a4
Ella Long <sla554@sfu.ca>
2026-02-06 15:20:32 -0800

    init: add Maven structure with Main and JUnit test

---
+++ src/test/java/solver/SanityTest.java
@@ -0,0 +1,11 @@
+package solver;
+
+import org.junit.jupiter.api.Test;
+import static  org.junit.jupiter.api.Assertions.*;
+
+public class SanityTest {
+    @Test
+    void sanity() {
+        assertEquals(2, 1 + 1);
+    }
+}
